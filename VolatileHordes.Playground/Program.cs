73a44f2ae6c6828ad60c62fd2335d35a9bd741f9
Troy <troyswanson1010@gmail.com>
2021-10-03 01:26:09 -0500

    Playground`PointDistanceAwayByAngle

--- VolatileHordes.Playground/Program.cs
+++ VolatileHordes.Playground/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 using VolatileHordes.Randomization;
 using VolatileHordes.Utility;
 
@@ -16,22 +17,31 @@ namespace VolatileHordes.Playground
         {
             var randomSource = new RandomSource();
             var pointService = new PointService(randomSource);
-            pointService.RandomlyAdjustAngle(10, 45)
-                .PrintLn("RandomlyAdjustAngle 10, 45");
-            pointService.RandomlyAdjustAngle(10, 45)
-                .PrintLn("RandomlyAdjustAngle 10, 45");
-            pointService.RandomlyAdjustAngle(10, 45)
-                .PrintLn("RandomlyAdjustAngle 10, 45");
-            pointService.RandomlyAdjustAngle(10, 45)
-                .PrintLn("RandomlyAdjustAngle 10, 45");
 
-            //for (int i = 0; i < 100; i++)
-            //{
-            //    randomSource.NextBool()
-            //        .PrintLn("NextBool");
-            //}
-
-            // pointService.PointDistanceAway();
+            pointService.PointDistanceAwayByAngle(
+                point: new PointF(8, 4),
+                angle: 0,
+                distance: 5
+            )
+                .PrintLn("PointDistanceAwayByAngle angle: 0");
+            pointService.PointDistanceAwayByAngle(
+                point: new PointF(8, 4),
+                angle: 45,
+                distance: 5
+            )
+                .PrintLn("PointDistanceAwayByAngle angle: 45");
+            pointService.PointDistanceAwayByAngle(
+                point: new PointF(8, 4),
+                angle: 90,
+                distance: 5
+            )
+                .PrintLn("PointDistanceAwayByAngle angle: 90");
+            pointService.PointDistanceAwayByAngle(
+                point: new PointF(8, 4),
+                angle: 180,
+                distance: 5
+            )
+                .PrintLn("PointDistanceAwayByAngle angle: 180");
 
             Console.ReadLine();
         }
