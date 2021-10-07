c2c7c8b052ec86554b34eda45535ceb704277fae
Justin Swanson <justin.c.swanson@gmail.com>
2021-10-06 19:07:52 -0500

    Fix for probability list

--- VolatileHordes/Probability/ProbabilityList.cs
+++ VolatileHordes/Probability/ProbabilityList.cs
@@ -6,13 +6,13 @@ namespace VolatileHordes.Probability
 {
     public class ProbabilityList<T>
     {
-        private double _runningTotal;
+        public UDouble TotalWeight { get; private set; }
         private readonly SortingListDictionary<double, T> _items = new();
 
         public void Add(T item, UDouble weight)
         {
-            _runningTotal += weight.Value;
-            _items[_runningTotal] = item;
+            TotalWeight = new UDouble(weight.Value + TotalWeight.Value);
+            _items[TotalWeight.Value] = item;
         }
 
         public T Get(RandomSource random)
@@ -21,8 +21,13 @@ namespace VolatileHordes.Probability
             {
                 throw new KeyNotFoundException("No items on probability list");
             }
-            var roll = random.NextDouble(_runningTotal);
-            if (!_items.TryGetInDirection(roll, higher: false, out var result))
+            var roll = random.NextDouble(TotalWeight.Value);
+            return GetAt(new UDouble(roll));
+        }
+
+        public T GetAt(UDouble roll)
+        {
+            if (!_items.TryGetInDirection(roll.Value, higher: true, out var result))
             {
                 throw new KeyNotFoundException("Could not find item for roll");
             }
