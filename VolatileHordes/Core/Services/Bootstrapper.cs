0454daabc899e13ed97f6868ac78f307d1654565
Troy <troyswanson1010@gmail.com>
2021-10-16 19:02:00 -0500

    PlayerGameEventsWrapper

--- VolatileHordes/Core/Services/Bootstrapper.cs
+++ VolatileHordes/Core/Services/Bootstrapper.cs
@@ -13,7 +13,9 @@ namespace VolatileHordes
             ModEvents.GameUpdate.RegisterHandler(GameUpdate);
             ModEvents.GameShutdown.RegisterHandler(GameShutdown);
             ModEvents.PlayerSpawnedInWorld.RegisterHandler(Container.PlayerZoneManager.PlayerSpawnedInWorld);
+            ModEvents.PlayerSpawnedInWorld.RegisterHandler(Container.PlayerGameEventsWrapper.PlayerSpawnedInWorld);
             ModEvents.PlayerDisconnected.RegisterHandler(Container.PlayerZoneManager.PlayerDisconnected);
+            ModEvents.PlayerDisconnected.RegisterHandler(Container.PlayerGameEventsWrapper.PlayerDisconnected);
         }
 
         private static ReplaySubject<Unit> _gameStarted = new();
---
+++ VolatileHordes/Players/PlayerGameEventsWrapper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Reactive.Subjects;
+
+namespace VolatileHordes.Players
+{
+    public class PlayerGameEventsWrapper
+    {
+        public PlayerGameEventsWrapper()
+        {
+
+        }
+
+        // # Input
+        public void PlayerSpawnedInWorld(ClientInfo? _cInfo, RespawnType _respawnReason, Vector3i _pos)
+        {
+            Logger.Debug("PlayerSpawnedInWorld \"{0}\", \"{1}\", \"{2}\"", _cInfo?.ToString() ?? "null", _respawnReason, _pos);
+            switch (_respawnReason)
+            {
+                case RespawnType.NewGame:
+                // case RespawnType.LoadedGame:
+                case RespawnType.EnterMultiplayer:
+                case RespawnType.JoinMultiplayer:
+                    PlayerAdded.OnNext(
+                        GetPlayerEntityId(_cInfo)
+                    );
+                    break;
+            }
+        }
+
+        public void PlayerDisconnected(ClientInfo? _cInfo, bool _bShutdown)
+        {
+            Logger.Debug("PlayerDisconnected \"{0}\", \"{1}\"", _cInfo?.ToString() ?? "null", _bShutdown);
+            PlayerRemoved.OnNext(
+                GetPlayerEntityId(_cInfo)
+            );
+        }
+
+        // # Internal
+        static int GetPlayerEntityId(ClientInfo? _cInfo)
+        {
+            if (_cInfo != null)
+                return _cInfo.entityId;
+
+            // On a local host this is set to null, grab id from player list.
+            var player = GameManager.Instance.World.Players.list[0];
+
+            return player.entityId;
+        }
+
+        // # Output
+        public Subject<int> PlayerAdded { get; } = new();
+        public Subject<int> PlayerRemoved { get; } = new();
+    }
+}
