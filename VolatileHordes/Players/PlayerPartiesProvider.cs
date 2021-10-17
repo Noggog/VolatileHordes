c990054865d6244d9f3ca3d5bba3eb273cf6e187
Troy <troyswanson1010@gmail.com>
2021-10-17 17:56:15 -0500

    cleanup

--- VolatileHordes/Players/PlayerPartiesProvider.cs
+++ VolatileHordes/Players/PlayerPartiesProvider.cs
@@ -17,18 +17,17 @@ namespace VolatileHordes.Players
         )
         {
             playerParties.Add(new(gameStageCalculator));
-            var playerIDs =
-                Observable.Merge(
-                    playerGameEventsWrapper.PlayerAdded.Select(x => new ValueTuple<bool, int>(true, x)),
-                    playerGameEventsWrapper.PlayerRemoved.Select(x => new ValueTuple<bool, int>(false, x))
-                )
-                .Subscribe(x =>
-                {
-                    if (x.Item1)
-                        playerParties[0].players.Add(x.Item2, new Player(world, x.Item2));
-                    else
-                        playerParties[0].players.Remove(x.Item2);
-                });
+            Observable.Merge(
+                playerGameEventsWrapper.PlayerAdded.Select(x => new ValueTuple<bool, int>(true, x)),
+                playerGameEventsWrapper.PlayerRemoved.Select(x => new ValueTuple<bool, int>(false, x))
+            )
+            .Subscribe(x =>
+            {
+                if (x.Item1)
+                    playerParties[0].players.Add(x.Item2, new Player(world, x.Item2));
+                else
+                    playerParties[0].players.Remove(x.Item2);
+            });
         }
     }
 }
