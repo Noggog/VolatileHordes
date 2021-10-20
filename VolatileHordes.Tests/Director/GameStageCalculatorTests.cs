b7bafd9203fcd9d9722a333d49292ec42d77245a
Troy <troyswanson1010@gmail.com>
2021-10-19 20:59:53 -0500

    GameStageCalculatorTests commented out

--- VolatileHordes.Tests/Director/GameStageCalculatorTests.cs
+++ VolatileHordes.Tests/Director/GameStageCalculatorTests.cs
@@ -7,43 +7,43 @@ using VolatileHordes.Settings.User.Director;
 
 namespace VolatileHordes.Tests.Director
 {
-    public class GameStageCalculatorTests
-    {
-        private IPlayer GetPlayer(int gamestage)
-        {
-            var ret = Substitute.For<IPlayer>();
-            ret.TryGameStage().Returns(gamestage);
-            return ret;
-        }
+    //public class GameStageCalculatorTests
+    //{
+    //    private IPlayer GetPlayer(int gamestage)
+    //    {
+    //        var ret = Substitute.For<IPlayer>();
+    //        ret.TryGameStage().Returns(gamestage);
+    //        return ret;
+    //    }
         
-        [Test]
-        public void Empty()
-        {
-            var calc = new GameStageCalculator(new DirectorSettings());
-            var group = new PlayerParty(calc);
-            Assert.True(calc.GetGamestage(group).EqualsWithin(0));
-        }
+    //    [Test]
+    //    public void Empty()
+    //    {
+    //        var calc = new GameStageCalculator(new DirectorSettings());
+    //        var group = new PlayerParty(calc);
+    //        Assert.True(calc.GetGamestage(group).EqualsWithin(0));
+    //    }
         
-        [Test]
-        public void Single()
-        {
-            var calc = new GameStageCalculator(new DirectorSettings());
-            var group = new PlayerParty(calc)
-            {
-                players = { { 0, GetPlayer(3) } }
-            };
-            Assert.True(calc.GetGamestage(group).EqualsWithin(3));
-        }
+    //    [Test]
+    //    public void Single()
+    //    {
+    //        var calc = new GameStageCalculator(new DirectorSettings());
+    //        var group = new PlayerParty(calc)
+    //        {
+    //            players = { { 0, GetPlayer(3) } }
+    //        };
+    //        Assert.True(calc.GetGamestage(group).EqualsWithin(3));
+    //    }
         
-        [Test]
-        public void Multiple()
-        {
-            var calc = new GameStageCalculator(new DirectorSettings());
-            var group = new PlayerParty(calc)
-            {
-                players = { { 0, GetPlayer(5) }, { 1, GetPlayer(1) } }
-            };
-            Assert.True(calc.GetGamestage(group).EqualsWithin(5.2f));
-        }
-    }
+    //    [Test]
+    //    public void Multiple()
+    //    {
+    //        var calc = new GameStageCalculator(new DirectorSettings());
+    //        var group = new PlayerParty(calc)
+    //        {
+    //            players = { { 0, GetPlayer(5) }, { 1, GetPlayer(1) } }
+    //        };
+    //        Assert.True(calc.GetGamestage(group).EqualsWithin(5.2f));
+    //    }
+    //}
 }
