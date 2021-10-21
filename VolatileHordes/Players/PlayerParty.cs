fa35ac25406ef5606a3ae4847542c73740657098
Troy <troyswanson1010@gmail.com>
2021-10-20 20:18:06 -0500

    moved code from PlayersAggregate to PlayerParty b/c idk about inheritanc

--- VolatileHordes/Players/PlayerParty.cs
+++ VolatileHordes/Players/PlayerParty.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
 using VolatileHordes.Director;
 using VolatileHordes.GameAbstractions;
 
@@ -15,6 +17,28 @@ namespace VolatileHordes.Players
         public PlayerParty(GameStageCalculator gameStageCalculator)
         {
             _gameStageCalculator = gameStageCalculator;
+        }
+
+        public IEnumerable<RectangleF> GetConnectedSpawnRects()
+        {
+            return GetConnectedSpawnRects(players.First(), players.Skip(1), new HashSet<int>());
+        }
+
+        private IEnumerable<RectangleF> GetConnectedSpawnRects(Player playerI, IEnumerable<Player> players, HashSet<int> passedPlayers)
+        {
+            yield return playerI.SpawnRectangle;
+            passedPlayers.Add(playerI.EntityId);
+
+            foreach (var playerJ in players)
+            {
+                if (passedPlayers.Contains(playerJ.EntityId)) continue;
+                if (!playerJ.SpawnRectangle.IntersectsWith(playerI.SpawnRectangle)) continue;
+
+                foreach (var rhsRect in GetConnectedSpawnRects(playerJ, players, passedPlayers))
+                {
+                    yield return rhsRect;
+                }
+            }
         }
     }
 }
