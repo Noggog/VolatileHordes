a6b8aac6d91f58a49b232804f446090d4f0e1d33
Troy <troyswanson1010@gmail.com>
2021-10-20 20:11:04 -0500

    PlayersAggregate`created

---
+++ VolatileHordes/Players/PlayersAggregate.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using VolatileHordes.GameAbstractions;
+
+namespace VolatileHordes.Players
+{
+    public class PlayersAggregate
+    {
+        public IEnumerable<Player> players { get; }
+        public PlayersAggregate(IEnumerable<Player> players)
+        {
+            this.players = players;
+        }
+
+        public IEnumerable<RectangleF> GetConnectedSpawnRects()
+        {
+            return GetConnectedSpawnRects(players.First(), players.Skip(1), new HashSet<int>());
+        }
+
+        private IEnumerable<RectangleF> GetConnectedSpawnRects(Player playerI, IEnumerable<Player> players, HashSet<int> passedPlayers)
+        {
+            yield return playerI.SpawnRectangle;
+            passedPlayers.Add(playerI.EntityId);
+
+            foreach (var playerJ in players)
+            {
+                if (passedPlayers.Contains(playerJ.EntityId)) continue;
+                if (!playerJ.SpawnRectangle.IntersectsWith(playerI.SpawnRectangle)) continue;
+
+                foreach (var rhsRect in GetConnectedSpawnRects(playerJ, players, passedPlayers))
+                {
+                    yield return rhsRect;
+                }
+            }
+        }
+    }
+}
